fe5416e2b5b0d7d88e6a5bc300341afbe9ab4cd6
GenjurX <Jurgen.Xibrraku@fti.edu.al>
2023-01-26 17:14:02 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,76 @@
+import express, { response } from "express";
+import cors from "cors";
+import database from "../src/database.js";
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//route for searching your notes
+app.get("/api/search/:searchValue", async (req, res) => {
+  const searchValue = req.params.searchValue;
+  const result = await database.raw(
+    `select * from sites where name like '%${searchValue}%' or category like '%${searchValue}%' or status like '%${searchValue}%''`,
+  );
+  res.status(200);
+  res.json(result);
+});
+
+//create a new jobsite
+app.post("/api/sites", async (req, res) => {
+  const { category, name, status } = req.body;
+  const insert = await database.raw(
+    `insert into sites (category, name, status) values ('${category}','${name}', '${status}')`,
+  );
+  const id = insert.lastInsertRowid;
+  const result = await database.raw(`select * from sites where id= ${id}`);
+  res.status(200);
+  res.json(result);
+});
+
+//get all jobsites
+app.get("/api/sites", async (req, res) => {
+  const result = await database.raw(`select * from sites`);
+  res.status(200);
+  res.json(result);
+});
+
+//get one single note
+app.get("/api/sites/:id", async (req, res) => {
+  const id = req.params.id;
+  const result = await database.raw(`select * from sites where id='${id}'`);
+  res.status(200);
+  res.json(result);
+});
+
+//update note
+app.put("/api/site/:id", async (req, res) => {
+  const id = req.params.id;
+  const { category, name, status } = req.body;
+  await database.raw(`update sites set name='${name}', category='${category}', status='${status}' where id=${id}`);
+  const result = await database.raw(`select * from sites where id= ${id}`);
+  res.status(200);
+  res.json(result);
+});
+
+//delete note
+app.delete("/api/site/:id", async (request, response) => {
+  const id = request.params.id;
+  await database.raw(`delete from sites where id=${id}`);
+  const result = await database.raw(`select * from sites`);
+  response.status(200);
+  response.json(result);
+});
+
+//Route that handles every other route
+app.all("/*", async (request, response) => {
+  response.status(404);
+  response.json({ error: "This route does not exist" });
+});
+
+const hostname = "localhost";
+const port = 4000;
+
+app.listen(port, hostname, () => {
+  console.log(`Server listening on http://${hostname}:${port}`);
+});
